03bc20334d5d17a0d933841f7537e0f3702fd9d3
funkYzWo <angelkozlev@gmail.com>
2019-03-22 16:50:10 +0000

    libPD working

---
+++ Assets/Scripts/TestSendToReceiver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSendToReceiver : MonoBehaviour
+{
+    public LibPdInstance pdPatch;
+    public float num;
+
+    public void Test() 
+    {
+        pdPatch.SendFloat("variable", num);
+    }
+}
